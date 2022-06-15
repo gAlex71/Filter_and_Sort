f0c26bc92b411384dae1e5cca27334174eaef3f5
gAlex71 <alexvladimirovich71@mail.ru>
2022-06-15 15:45:51 +0300

    Second_commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { useMemo, useState } from 'react';
 import './App.css';
 import TableItem from './components/TableItem';
-import MyFilter from './UI/filter/MyFilter';
 import MyInput from './UI/input/MyInput';
 import MyPagination from './UI/pagination/MyPagination';
 import MySelect from './UI/select/MySelect';
@@ -9,20 +8,20 @@ import MySelect from './UI/select/MySelect';
 function App() {
   //Массив элементов таблицы
   const [items] = useState ([
-    {id:1, data: '01.09.2017', name: 'Spar', amount: 3, distance: 2500},
-    {id:2, data: '14.08.2011', name: 'Zara', amount: 2, distance: 1000},
-    {id:3, data: '23.10.2009', name: 'Colins', amount: 5, distance: 300},
-    {id:4, data: '02.06.2015', name: 'Nike', amount: 1, distance: 2300},
-    {id:5, data: '18.04.2019', name: 'Adidas', amount: 3, distance: 3000},
-    {id:6, data: '05.12.2020', name: 'Puma', amount: 2, distance: 200},
-    {id:7, data: '28.02.2016', name: 'Ozon', amount: 5, distance: 5000},
-    {id:8, data: '05.06.2018', name: 'Lamoda', amount: 1, distance: 2400},
-    {id:9, data: '24.09.2020', name: 'Rebook', amount: 3, distance: 6000},
-    {id:10, data: '18.09.2017', name: 'McDonalds', amount: 2, distance: 300},
-    {id:11, data: '28.02.2016', name: 'Magnit', amount: 15, distance: 100},
-    {id:12, data: '05.06.2018', name: 'Perectrestoc', amount: 8, distance: 300},
-    {id:13, data: '24.09.2020', name: 'Apple', amount: 2, distance: 6000},
-    {id:14, data: '18.09.2017', name: 'Mi', amount: 4, distance: 800}
+    {id:1, data: '01.09.2017', name: 'Spar', amount: '3', distance: '2500'},
+    {id:2, data: '14.08.2011', name: 'Zara', amount: '2', distance: '1000'},
+    {id:3, data: '23.10.2009', name: 'Colins', amount: '5', distance: '300'},
+    {id:4, data: '02.06.2015', name: 'Nike', amount: '1', distance: '2300'},
+    {id:5, data: '18.04.2019', name: 'Adidas', amount: '3', distance: '3000'},
+    {id:6, data: '05.12.2020', name: 'Puma', amount: '2', distance: '200'},
+    {id:7, data: '28.02.2016', name: 'Ozon', amount: '5', distance: '5000'},
+    {id:8, data: '05.06.2018', name: 'Lamoda', amount: '1', distance: '2400'},
+    {id:9, data: '24.09.2020', name: 'Rebook', amount: '3', distance: '6000'},
+    {id:10, data: '18.09.2017', name: 'McDonalds', amount: '2', distance: '300'},
+    {id:11, data: '28.02.2016', name: 'Magnit', amount: '15', distance: '100'},
+    {id:12, data: '05.06.2018', name: 'Perectrestoc', amount: '8', distance: '300'},
+    {id:13, data: '24.09.2020', name: 'Apple', amount: '2', distance: '6000'},
+    {id:14, data: '18.09.2017', name: 'Mi', amount: '4', distance: '800'}
   ])
 
   const [selectedSort, setSelectedSort] = useState('')
@@ -48,8 +47,12 @@ function App() {
   }, [selectedSort, items])
 
   //Отсортированый и отфильтрованный массив
+  // const sortedAndSearched = useMemo(() => {
+  //   return sortedItems.filter(item => item.name.toLowerCase().includes(searchQuery))
+  // }, [searchQuery,filter, sortedItems])
+
   const sortedAndSearched = useMemo(() => {
-    return sortedItems.filter(item => item.name.toLowerCase().includes(searchQuery))
+    return sortedItems.filter(item => item.distance.includes(searchQuery))
   }, [searchQuery,filter, sortedItems])
 
   //Текущие элементы, отображаемые на данной странице
